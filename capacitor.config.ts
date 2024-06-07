bdb82500457eb05c9a66b666d02cd9e354735ea5
umeshchatla <umesh.chatla@excelli.com>
2024-06-07 18:45:46 +0530

    Initial capacitorjs setup add capacitorjs android add capacitorjs ios

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oze.app',
+  appName: 'oze-website-development',
+  webDir: 'build'
+};
+
+export default config;
