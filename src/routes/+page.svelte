b5cfc69d6bf74ae8cd147bd5b34f187a01595a3e
radiorambo <64721943+radiorambo@users.noreply.github.com>
2024-06-21 11:14:20 +0530

    setup inlang with small demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import * as m from '$lib/paraglide/messages.js';
+	import { languageTag } from '$lib/paraglide/runtime.js';
+	let language: 'en' | 'de' = $state(languageTag());
+
+	$effect(() => {
+		if (language === 'en') {
+			goto('/');
+		}
+		if (language !== 'en' && languageTag() !== $page.url.pathname) {
+			goto(language);
+		}
+	});
+</script>
+
+<select bind:value={language}>
+	<option value="en">English</option>
+	<option value="de">German</option>
+</select>
+<h1>{m.title()}</h1>
+<p>{m.description()}</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import { paraglide } from '@inlang/paraglide-sveltekit/vite'
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),sveltekit(), tailwindcss()]
 });
